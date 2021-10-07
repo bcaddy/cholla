e1777694d2f662cd438cb644a98d97a2f13ff1e0
Bob Caddy <r.Caddy@pitt.edu>
2021-10-07 11:52:33 -0400

    Preliminary C-3PO support

---
+++ builds/setup.c3po.gcc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#-- This script needs to be sourced in the terminal, e.g.
+#   source ./setup.c3po.gcc.sh
+
+
+# export GCC_ROOT=$GCC_UMS_DIR
+
+# echo "Using GCC in $GCC_ROOT"
+
+# export PATH=$GCC_ROOT/bin:${PATH}
+
+# export OMPI_CC=${GCC_ROOT}/bin/gcc
+# export OMPI_CXX=${GCC_ROOT}/bin/g++
+# export OMPI_FC=${GCC_ROOT}/bin/gfortran
+# export LD_LIBRARY_PATH=${GCC_ROOT}/lib64:${LD_LIBRARY_PATH}
+
+# echo "mpicxx --version is: "
+# mpicxx --version
+
+# export MPI_GPU="-DMPI_GPU"
+# export F_OFFLOAD="-fopenmp -foffload=nvptx-none='-lm -Ofast'"
+
+export CHOLLA_MACHINE=c3po
+export CHOLLA_ENVSET=1
