c71632e7111df4469313a14a8e1eedabbf9517d8
Bob Caddy <r.Caddy@pitt.edu>
2024-04-29 16:58:30 -0400

    Restore fuction that got lost in rebase

--- src/reconstruction/ppm_cuda.cu
+++ src/reconstruction/ppm_cuda.cu
@@ -15,6 +15,302 @@
   #include "../utils/hydro_utilities.h"
 #endif
 
+void __device__ __host__ PPM_Characteristic_Evolution(hydro_utilities::Primitive const cell_i, Real const dt,
+                                                      Real const dx, Real const gamma,
+                                                      hydro_utilities::Primitive &interface_R_imh,
+                                                      hydro_utilities::Primitive &interface_L_iph)
+{
+  // This is the beginning of the characteristic tracing
+  // Step 8 - Compute the coefficients for the monotonized parabolic
+  // interpolation function
+  //          Stone Eqn 54
+  hydro_utilities::Primitive interface_slope;
+  interface_slope.density      = interface_L_iph.density - interface_R_imh.density;
+  interface_slope.velocity.x() = interface_L_iph.velocity.x() - interface_R_imh.velocity.x();
+  interface_slope.velocity.y() = interface_L_iph.velocity.y() - interface_R_imh.velocity.y();
+  interface_slope.velocity.z() = interface_L_iph.velocity.z() - interface_R_imh.velocity.z();
+  interface_slope.pressure     = interface_L_iph.pressure - interface_R_imh.pressure;
+
+  Real const d_6  = 6.0 * (cell_i.density - 0.5 * (interface_R_imh.density + interface_L_iph.density));
+  Real const vx_6 = 6.0 * (cell_i.velocity.x() - 0.5 * (interface_R_imh.velocity.x() + interface_L_iph.velocity.x()));
+  Real const vy_6 = 6.0 * (cell_i.velocity.y() - 0.5 * (interface_R_imh.velocity.y() + interface_L_iph.velocity.y()));
+  Real const vz_6 = 6.0 * (cell_i.velocity.z() - 0.5 * (interface_R_imh.velocity.z() + interface_L_iph.velocity.z()));
+  Real const p_6  = 6.0 * (cell_i.pressure - 0.5 * (interface_R_imh.pressure + interface_L_iph.pressure));
+
+#ifdef DE
+  interface_slope.gas_energy_specific = interface_L_iph.gas_energy_specific - interface_R_imh.gas_energy_specific;
+  Real const ge_6                     = 6.0 * (cell_i.gas_energy_specific -
+                           0.5 * (interface_R_imh.gas_energy_specific + interface_L_iph.gas_energy_specific));
+#endif  // DE
+
+#ifdef SCALAR
+  Real scalar_6[NSCALARS];
+  for (int i = 0; i < NSCALARS; i++) {
+    interface_slope.scalar_specific[i] = interface_L_iph.scalar_specific[i] - interface_R_imh.scalar_specific[i];
+    scalar_6[i]                        = 6.0 * (cell_i.scalar_specific[i] -
+                         0.5 * (interface_R_imh.scalar_specific[i] + interface_L_iph.scalar_specific[i]));
+  }
+#endif  // SCALAR
+
+  // Compute the eigenvalues of the linearized equations in the
+  // primitive variables using the cell-centered primitive variables
+
+  // recalculate the adiabatic sound speed in cell i
+  Real const sound_speed = hydro_utilities::Calc_Sound_Speed(cell_i.pressure, cell_i.density, gamma);
+
+  Real const lambda_m = cell_i.velocity.x() - sound_speed;
+  Real const lambda_0 = cell_i.velocity.x();
+  Real const lambda_p = cell_i.velocity.x() + sound_speed;
+
+  // Step 9 - Compute the left and right interface values using monotonized
+  // parabolic interpolation
+  //          Stone Eqns 55 & 56
+
+  // largest eigenvalue
+  Real const lambda_max = fmax(lambda_p, (Real)0);
+  // smallest eigenvalue
+  Real const lambda_min = fmin(lambda_m, (Real)0);
+
+  // left interface value, i+1/2
+  Real const dtodx = dt / dx;
+  interface_L_iph.density =
+      interface_L_iph.density -
+      lambda_max * (0.5 * dtodx) * (interface_slope.density - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * d_6);
+  interface_L_iph.velocity.x() =
+      interface_L_iph.velocity.x() -
+      lambda_max * (0.5 * dtodx) * (interface_slope.velocity.x() - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * vx_6);
+  interface_L_iph.velocity.y() =
+      interface_L_iph.velocity.y() -
+      lambda_max * (0.5 * dtodx) * (interface_slope.velocity.y() - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * vy_6);
+  interface_L_iph.velocity.z() =
+      interface_L_iph.velocity.z() -
+      lambda_max * (0.5 * dtodx) * (interface_slope.velocity.z() - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * vz_6);
+  interface_L_iph.pressure =
+      interface_L_iph.pressure -
+      lambda_max * (0.5 * dtodx) * (interface_slope.pressure - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * p_6);
+
+  // right interface value, i-1/2
+  interface_R_imh.density =
+      interface_R_imh.density -
+      lambda_min * (0.5 * dtodx) * (interface_slope.density + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * d_6);
+  interface_R_imh.velocity.x() =
+      interface_R_imh.velocity.x() -
+      lambda_min * (0.5 * dtodx) * (interface_slope.velocity.x() + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * vx_6);
+  interface_R_imh.velocity.y() =
+      interface_R_imh.velocity.y() -
+      lambda_min * (0.5 * dtodx) * (interface_slope.velocity.y() + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * vy_6);
+  interface_R_imh.velocity.z() =
+      interface_R_imh.velocity.z() -
+      lambda_min * (0.5 * dtodx) * (interface_slope.velocity.z() + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * vz_6);
+  interface_R_imh.pressure =
+      interface_R_imh.pressure -
+      lambda_min * (0.5 * dtodx) * (interface_slope.pressure + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * p_6);
+
+#ifdef DE
+  interface_L_iph.gas_energy_specific =
+      interface_L_iph.gas_energy_specific -
+      lambda_max * (0.5 * dtodx) *
+          (interface_slope.gas_energy_specific - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * ge_6);
+  interface_R_imh.gas_energy_specific =
+      interface_R_imh.gas_energy_specific -
+      lambda_min * (0.5 * dtodx) *
+          (interface_slope.gas_energy_specific + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * ge_6);
+#endif  // DE
+
+#ifdef SCALAR
+  for (int i = 0; i < NSCALARS; i++) {
+    interface_L_iph.scalar_specific[i] =
+        interface_L_iph.scalar_specific[i] -
+        lambda_max * (0.5 * dtodx) *
+            (interface_slope.scalar_specific[i] - (1.0 - (2.0 / 3.0) * lambda_max * dtodx) * scalar_6[i]);
+    interface_R_imh.scalar_specific[i] =
+        interface_R_imh.scalar_specific[i] -
+        lambda_min * (0.5 * dtodx) *
+            (interface_slope.scalar_specific[i] + (1.0 + (2.0 / 3.0) * lambda_min * dtodx) * scalar_6[i]);
+  }
+#endif  // SCALAR
+
+  // Step 10 - Perform the characteristic tracing
+  //           Stone Eqns 57 - 60
+
+  // left-hand interface value, i+1/2
+  Real sum_1 = 0, sum_2 = 0, sum_3 = 0, sum_4 = 0, sum_5 = 0;
+#ifdef DE
+  Real sum_ge = 0;
+  Real chi_ge = 0;
+#endif  // DE
+#ifdef SCALAR
+  Real chi_scalar[NSCALARS];
+  Real sum_scalar[NSCALARS];
+  for (Real &val : sum_scalar) {
+    val = 0;
+  }
+#endif  // SCALAR
+
+  if (lambda_m >= 0) {
+    Real const A = (0.5 * dtodx) * (lambda_p - lambda_m);
+    Real const B = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_p * lambda_p - lambda_m * lambda_m);
+
+    Real const chi_1 = A * (interface_slope.density - d_6) + B * d_6;
+    Real const chi_2 = A * (interface_slope.velocity.x() - vx_6) + B * vx_6;
+    Real const chi_3 = A * (interface_slope.velocity.y() - vy_6) + B * vy_6;
+    Real const chi_4 = A * (interface_slope.velocity.z() - vz_6) + B * vz_6;
+    Real const chi_5 = A * (interface_slope.pressure - p_6) + B * p_6;
+
+    sum_1 += -0.5 * (cell_i.density * chi_2 / sound_speed - chi_5 / (sound_speed * sound_speed));
+    sum_2 += 0.5 * (chi_2 - chi_5 / (sound_speed * cell_i.density));
+    sum_5 += -0.5 * (cell_i.density * chi_2 * sound_speed - chi_5);
+  }
+  if (lambda_0 >= 0) {
+    Real const A = (0.5 * dtodx) * (lambda_p - lambda_0);
+    Real const B = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_p * lambda_p - lambda_0 * lambda_0);
+
+    Real const chi_1 = A * (interface_slope.density - d_6) + B * d_6;
+    Real const chi_2 = A * (interface_slope.velocity.x() - vx_6) + B * vx_6;
+    Real const chi_3 = A * (interface_slope.velocity.y() - vy_6) + B * vy_6;
+    Real const chi_4 = A * (interface_slope.velocity.z() - vz_6) + B * vz_6;
+    Real const chi_5 = A * (interface_slope.pressure - p_6) + B * p_6;
+#ifdef DE
+    chi_ge = A * (interface_slope.gas_energy_specific - ge_6) + B * ge_6;
+#endif  // DE
+#ifdef SCALAR
+    for (int i = 0; i < NSCALARS; i++) {
+      chi_scalar[i] = A * (interface_slope.scalar_specific[i] - scalar_6[i]) + B * scalar_6[i];
+    }
+#endif  // SCALAR
+
+    sum_1 += chi_1 - chi_5 / (sound_speed * sound_speed);
+    sum_3 += chi_3;
+    sum_4 += chi_4;
+#ifdef DE
+    sum_ge += chi_ge;
+#endif  // DE
+#ifdef SCALAR
+    for (int i = 0; i < NSCALARS; i++) {
+      sum_scalar[i] += chi_scalar[i];
+    }
+#endif  // SCALAR
+  }
+  if (lambda_p >= 0) {
+    Real const A = (0.5 * dtodx) * (lambda_p - lambda_p);
+    Real const B = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_p * lambda_p - lambda_p * lambda_p);
+
+    Real const chi_1 = A * (interface_slope.density - d_6) + B * d_6;
+    Real const chi_2 = A * (interface_slope.velocity.x() - vx_6) + B * vx_6;
+    Real const chi_3 = A * (interface_slope.velocity.y() - vy_6) + B * vy_6;
+    Real const chi_4 = A * (interface_slope.velocity.z() - vz_6) + B * vz_6;
+    Real const chi_5 = A * (interface_slope.pressure - p_6) + B * p_6;
+
+    sum_1 += 0.5 * (cell_i.density * chi_2 / sound_speed + chi_5 / (sound_speed * sound_speed));
+    sum_2 += 0.5 * (chi_2 + chi_5 / (sound_speed * cell_i.density));
+    sum_5 += 0.5 * (cell_i.density * chi_2 * sound_speed + chi_5);
+  }
+
+  // add the corrections to the initial guesses for the interface values
+  interface_L_iph.density += sum_1;
+  interface_L_iph.velocity.x() += sum_2;
+  interface_L_iph.velocity.y() += sum_3;
+  interface_L_iph.velocity.z() += sum_4;
+  interface_L_iph.pressure += sum_5;
+#ifdef DE
+  interface_L_iph.gas_energy_specific += sum_ge;
+#endif  // DE
+#ifdef SCALAR
+  for (int i = 0; i < NSCALARS; i++) {
+    interface_L_iph.scalar_specific[i] += sum_scalar[i];
+  }
+#endif  // SCALAR
+
+  // right-hand interface value, i-1/2
+  sum_1 = 0;
+  sum_2 = 0;
+  sum_3 = 0;
+  sum_4 = 0;
+  sum_5 = 0;
+#ifdef DE
+  sum_ge = 0;
+#endif  // DE
+#ifdef SCALAR
+  for (Real &val : sum_scalar) {
+    val = 0;
+  }
+#endif  // SCALAR
+  if (lambda_m <= 0) {
+    Real const C = (0.5 * dtodx) * (lambda_m - lambda_m);
+    Real const D = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_m * lambda_m - lambda_m * lambda_m);
+
+    Real const chi_1 = C * (interface_slope.density + d_6) + D * d_6;
+    Real const chi_2 = C * (interface_slope.velocity.x() + vx_6) + D * vx_6;
+    Real const chi_3 = C * (interface_slope.velocity.y() + vy_6) + D * vy_6;
+    Real const chi_4 = C * (interface_slope.velocity.z() + vz_6) + D * vz_6;
+    Real const chi_5 = C * (interface_slope.pressure + p_6) + D * p_6;
+
+    sum_1 += -0.5 * (cell_i.density * chi_2 / sound_speed - chi_5 / (sound_speed * sound_speed));
+    sum_2 += 0.5 * (chi_2 - chi_5 / (sound_speed * cell_i.density));
+    sum_5 += -0.5 * (cell_i.density * chi_2 * sound_speed - chi_5);
+  }
+  if (lambda_0 <= 0) {
+    Real const C = (0.5 * dtodx) * (lambda_m - lambda_0);
+    Real const D = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_m * lambda_m - lambda_0 * lambda_0);
+
+    Real const chi_1 = C * (interface_slope.density + d_6) + D * d_6;
+    Real const chi_2 = C * (interface_slope.velocity.x() + vx_6) + D * vx_6;
+    Real const chi_3 = C * (interface_slope.velocity.y() + vy_6) + D * vy_6;
+    Real const chi_4 = C * (interface_slope.velocity.z() + vz_6) + D * vz_6;
+    Real const chi_5 = C * (interface_slope.pressure + p_6) + D * p_6;
+#ifdef DE
+    chi_ge = C * (interface_slope.gas_energy_specific + ge_6) + D * ge_6;
+#endif  // DE
+#ifdef SCALAR
+    for (int i = 0; i < NSCALARS; i++) {
+      chi_scalar[i] = C * (interface_slope.scalar_specific[i] + scalar_6[i]) + D * scalar_6[i];
+    }
+#endif  // SCALAR
+
+    sum_1 += chi_1 - chi_5 / (sound_speed * sound_speed);
+    sum_3 += chi_3;
+    sum_4 += chi_4;
+#ifdef DE
+    sum_ge += chi_ge;
+#endif  // DE
+#ifdef SCALAR
+    for (int i = 0; i < NSCALARS; i++) {
+      sum_scalar[i] += chi_scalar[i];
+    }
+#endif  // SCALAR
+  }
+  if (lambda_p <= 0) {
+    Real const C = (0.5 * dtodx) * (lambda_m - lambda_p);
+    Real const D = (1.0 / 3.0) * (dtodx) * (dtodx) * (lambda_m * lambda_m - lambda_p * lambda_p);
+
+    Real const chi_1 = C * (interface_slope.density + d_6) + D * d_6;
+    Real const chi_2 = C * (interface_slope.velocity.x() + vx_6) + D * vx_6;
+    Real const chi_3 = C * (interface_slope.velocity.y() + vy_6) + D * vy_6;
+    Real const chi_4 = C * (interface_slope.velocity.z() + vz_6) + D * vz_6;
+    Real const chi_5 = C * (interface_slope.pressure + p_6) + D * p_6;
+
+    sum_1 += 0.5 * (cell_i.density * chi_2 / sound_speed + chi_5 / (sound_speed * sound_speed));
+    sum_2 += 0.5 * (chi_2 + chi_5 / (sound_speed * cell_i.density));
+    sum_5 += 0.5 * (cell_i.density * chi_2 * sound_speed + chi_5);
+  }
+
+  // add the corrections
+  interface_R_imh.density += sum_1;
+  interface_R_imh.velocity.x() += sum_2;
+  interface_R_imh.velocity.y() += sum_3;
+  interface_R_imh.velocity.z() += sum_4;
+  interface_R_imh.pressure += sum_5;
+#ifdef DE
+  interface_R_imh.gas_energy_specific += sum_ge;
+#endif  // DE
+#ifdef SCALAR
+  for (int i = 0; i < NSCALARS; i++) {
+    interface_R_imh.scalar_specific[i] += sum_scalar[i];
+  }
+#endif  // SCALAR
+
+  // This is the end of the characteristic tracing
+}
 // =====================================================================================================================
 template <int dir>
 __global__ __launch_bounds__(TPB) void PPM_cuda(Real *dev_conserved, Real *dev_bounds_L, Real *dev_bounds_R, int nx,
